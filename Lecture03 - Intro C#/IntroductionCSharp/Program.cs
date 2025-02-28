a33d7fdcc1bdfcab7f4a34b20355b7889032b9f5
VittoriaFrau <vittoria.frau7@gmail.com>
2025-02-28 14:25:34 +0100

    changes in intro c#

--- Lecture03 - Intro C#/IntroductionCSharp/Program.cs
+++ Lecture03 - Intro C#/IntroductionCSharp/Program.cs
@@ -1,193 +1,33 @@
-﻿// Variables
+﻿using System.Collections;
 
-using System.Collections;
-using System.Globalization;
+string italianHi = "Ciao";
+string danishHi = "Hej";
+int myFavouriteNumber;
 
-int myNum = 5;
-double myDoubleNum = 5.99D;
-char myLetter = 'D';
-bool myBool = true;
-string myText = "Hello";
-float myFloat = 5.75F;
 
-string[] myBoardGames = {"Unlock", "Victorian Mastermind", "Wonderbook"};
-string[] myMovies = new string[3] {"The Lord of The Rings", "Dead Poets Society", "Hidden Figures"};
+int[] numbers = {1, 2, 3};
+int[] numbersSecond;
 
-//Access the elements
-Console.WriteLine(myMovies[0]);
+numbers = new[] { 3, 4, 5, 6 };
 
-// Change an element
-myBoardGames[0] = "Merchants Cove";
 
-// length
-Console.WriteLine(myBoardGames.Length);
+myFavouriteNumber = 7;
+Console.WriteLine(danishHi);
 
-//add an element to the array myboardgames
-Array.Resize(ref myBoardGames, 4);
-myBoardGames[3] = "Monopoli";
 
-ArrayList myArray = new ArrayList();
- 
-myArray.Add(12.56);
-myArray.Add("Hello World");
-myArray.Add(null);
-myArray.Add('V');
-myArray.Add(1234);
+ArrayList pets = new ArrayList();
+pets.Add("Baloo");
+pets.Add("Nemo");
+pets.Add("Line");
 
-foreach(var elements in myArray)
+pets.Sort();
+
+for (int i = 0; i < pets.Count; i++)
 {
-    Console.WriteLine(elements);
+    Console.WriteLine(pets[i]);
 }
 
-
-myArray.Remove('V');
-Console.WriteLine(myArray.Count); // 4
-myArray.Clear();
-myArray.Sort(); 
-
-string txt = "Hello World";
-Console.WriteLine("The length of the txt string is: " + txt.Length);
-Console.WriteLine(txt.ToUpper());   // Outputs "HELLO WORLD"
-Console.WriteLine(txt.ToLower());   // Outputs "hello world"
-
-Console.WriteLine("Hello " + "World"); //Concatenation through the + operator
-Console.WriteLine("Hello \n World"); //New line
-
-int year = 2025;
-
-if (year > 2015)
+foreach (var pet in pets)
 {
-    Console.WriteLine("Year is greater than 2015");
+    Console.WriteLine(pet);
 }
-else
-{
-    Console.WriteLine("Year is not greater than 2015");
-}
-
-Console.WriteLine(year > 2015 ? "Year is greater than 2015" : "Year is not greater than 2015");
-
-switch (year)
-{
-    case 2013 :
-        Console.WriteLine("It's 2013!");
-        break;
-    case 2012 :
-        Console.WriteLine("It's 2012!");
-        break;
-    default :
-        Console.WriteLine("It's " + year + "!");
-        break;
-}
-
-while (year >= 2013)
-{
-    // Code to be executed
-}
-
-do
-{
-    // Code to be executed
-} while(year >= 2013);
-
-string [] collection  = {"Hello", "World", "!"};
-
-for (int i = 0; i < collection.Length; i++)
-{
-    Console.WriteLine(i);
-}
-foreach (var item in collection)
-{
-    Console.WriteLine(item);
-}
-
-// example of a method
-int Sum(int a, int b)
-{
-    return a + b;
-}
-
-class User
-{
-    public string name;
-    public int age;
-    private string password;
-
-    public User(string name, int age)
-    {
-        this.name = name;
-        this.age = age;
-    }
-
-    public void Greet()
-    {
-        Console.WriteLine("Hello, my name is " + name);
-    }
-
-    public void SetPassword(string password)
-    {
-        
-    }
-
-}
-
-public class Boiler
-{
-
-    private int temperature;
-    private Boolean status; //true on, false off
-    public const int DEFAULT_TEMPERATURE = 25;
-
-    public Boiler()
-    {
-        temperature = DEFAULT_TEMPERATURE;
-        status = false;
-    }
-
-    public void turnOn()
-    {
-        if (status)
-        {
-            Console.WriteLine("Boiler is already on");
-            return;
-        }
-
-        status = true;
-    }
-
-    public void turnOff()
-    {
-        if (!status)
-        {
-            Console.WriteLine("Boiler is already off");
-            return;
-        }
-
-        status = false;
-    }
-
-    public void toggle()
-    {
-        status = !status;
-    }
-
-    public void increment()
-    {
-        increment(1);
-    }
-
-    public void increment(int n)
-    {
-        if (status)
-        {
-            temperature += n;
-        }
-    }
-
-    public void printInformation()
-    {
-        String statusString = status ? "accesa" : "spenta";
-        Console.WriteLine(
-            "Status: " + statusString + ". Temperature: " + temperature + ".");
-    }
-}
-
